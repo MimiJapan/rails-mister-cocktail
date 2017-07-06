9b1f682ff7076277f3ab7797cc4f7f3dfad811f5
Miyako Ando <miyako_hosikuzu@hotmail.com>
2017-07-06 16:35:02 +0900

    done with the backend

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,21 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :cocktails
+  resources :doses
+  resources :cocktails do
+    resources :doses, only: [ :new, :create]
+  end
 end
+
+
+
+
+
+
+
+
+#   resources :restaurants do
+#     resources :reviews, only: [ :new, :create ]
+
+# end
+#   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# end
